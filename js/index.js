c97adf4d655221a16c525adc60cef91245338d90
imon-sheikh <mdimonhossain2017@gmail.com>
2024-09-24 20:36:24 +0600

    calculate and calculate saving event completed

--- js/index.js
+++ js/index.js
@@ -1,38 +1,49 @@
 //Getting all the value by globally
-const income = parseFloat(document.getElementById('income').value)
-const software = parseFloat(document.getElementById('software').value)
-const courses = parseFloat(document.getElementById('courses').value)
-const internet = parseFloat(document.getElementById('internet').value)
 
 //add event listener for calculate Button
-const calculateButton = document.getElementById('calculate')
-calculateButton.addEventListener('click', function(){
-    // console.log(Kaj korche);
+const calculateButton = document.getElementById("calculate");
+calculateButton.addEventListener("click", function () {
+  // console.log(Kaj korche);
+  // console.table({income, software, courses, internet});
+  const income = parseFloat(document.getElementById("income").value);
+  const software = parseFloat(document.getElementById("software").value);
+  const courses = parseFloat(document.getElementById("courses").value);
+  const internet = parseFloat(document.getElementById("internet").value);
 
+  const totalExpenses = software + courses + internet;
+  const balance = income - totalExpenses;
 
-    // console.table({income, software, courses, internet});
+  // console.table({totalExpenses, balance});
+  const totalExpensesElement = document.getElementById("total-expenses");
+  totalExpensesElement.innerText = totalExpenses.toFixed(2);
 
-    const totalExpenses = software + courses + internet
-    const balance = income - totalExpenses
+  const balanceElement = document.getElementById("balance");
+  balanceElement.innerText = balance.toFixed(2);
 
-    // console.table({totalExpenses, balance});
-    
-    const totalExpensesElement = document.getElementById('total-expenses')
-    totalExpensesElement.innerText = totalExpenses.toFixed(2) 
-    
-    const balanceElement = document.getElementById('balance')
-    balanceElement.innerText = balance.toFixed(2)
+  const result = document.getElementById("results");
+  result.classList.remove("hidden");
+});
 
-    const result = document.getElementById('results')
-    result.classList.remove('hidden')
-})
+//add event listener for calculate Saving  Button2
+const calculateSavingButton = document.getElementById("calculate-savings");
+calculateSavingButton.addEventListener("click", function () {
+  const savingPercentage = parseFloat(document.getElementById("savings").value);
 
+  const income = parseFloat(document.getElementById("income").value);
+  const software = parseFloat(document.getElementById("software").value);
+  const courses = parseFloat(document.getElementById("courses").value);
+  const internet = parseFloat(document.getElementById("internet").value);
 
-//add event listener for calculate Saving  Button
-const calculateSavingButton = document.getElementById('calculate-savings')
-calculateSavingButton.addEventListener('click', function(){
+  const totalExpenses = software + courses + internet;
+  const balance = income - totalExpenses;
 
-const savingPercentage = parseFloat(document.getElementById('savings').value)
-console.log(savingPercentage);
+  const savingAmount = (savingPercentage * balance) / 100;
+  console.log(savingAmount);
 
-})
+  const savingElement = document.getElementById('savings-amount')
+  savingElement.innerText = savingAmount.toFixed(2)
+
+  const remainingBalance = balance - savingAmount
+  const remainingElement = document.getElementById('remaining-balance')
+  remainingElement.innerText = remainingBalance.toFixed(2)
+});
